1b04c919e405760634b1c6c97503881a1cd18bc5
Thatchayani <thatchayanikannan0110@gmail.com>
2025-04-07 11:57:05 +0530

    portfolio

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Designer", "UI/UX Designer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+    });
